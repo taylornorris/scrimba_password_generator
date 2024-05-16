67fb174d0ab327f7cef8d64e1ad37ac3321bd092
taylornorris <taylornorrisdev@gmail.com>
2024-05-16 08:46:30 -0600

    Finalize project

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-// when button is clicked, two random 15 char strings are generated
-// each string is stored in a variable
-// password fields are populated with varibles
-
-let string_one
-let string_two
 
 let btnEl = document.getElementById("generate_btn")
 let passwordOneEl = document.getElementById("password_one")
 let passwordTwoEl = document.getElementById("password_two")
 
-btnEl.addEventListener("click", generatePass)
+btnEl.addEventListener("click", populatePass)
+
+let passwordLength = 15
+
+function generateRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
 
 function generatePass() {
-    console.log("btn was clicked")
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += generateRandomCharacter()
+    }
+    return randomPassword
 }
+
+function populatePass() {
+    let passwordOne = generatePass()
+    let passwordTwo = generatePass()
+    
+    passwordOneEl.textContent = passwordOne
+    passwordTwoEl.textContent = passwordTwo
+}
+
+
 
